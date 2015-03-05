4152e2c4d86e0459a2982d76fbb7268f619923f8
cnreed <cnreed@usfca.edu>
2015-03-05 10:29:02 -0800

    Pushing changes to Game.js. Need to add Prototypes

--- justinBranch/Acquire/src/Game.js
+++ justinBranch/Acquire/src/Game.js
@@ -16,7 +16,7 @@ var Game,
     players
 
 
-
+/* Constructor */
 function Game(x ,y) {
 
     board = new Board(x, y);
@@ -41,6 +41,22 @@ function Game(x ,y) {
 
 }
 
+/*
+Game.prototype.start = function ();
+this.status = "playing";
+//create board
+//hand out tiles
+shuffle tiles
+ccreate Companies
+determine order
+
+module.exports = {
+ return new Game :function(params)
+
+}
+*/
+
+/* Look underscore */
 function getMove(playerIndex, board) {
 
     var player = players[playerIndex];
@@ -72,83 +88,109 @@ function getMove(playerIndex, board) {
 
 function checkAdjacency(tile, x, y) {
 
-    var compare;    //The compare tile
-    var results;
-    var company;
     board.checkBoundaries(tile, x, y);
 
-    if(tile.top) {
-        compare = board.getTile(x, y-1);
-        if(compare.status == "ONBOARD") {
-            if(compare.company == null) {
-                results = selectCompany(tile, compare);
-                console(results);
-            }
-            else {
-                company = compare.company;
-                company.add(compare);
-            }
+    if(tile.top)    checkHelper(x-1, y);
+    if(tile.left)   checkHelper(tile, x, y-1);;
+    if(tile.bottom) checkHelper(tile, x+1, y);
+    if(tile.right)  checkHelper(tile, x, y+1);
+}
+
+function checkHelper(tile, x, y) {
+
+    var company;
+    var results;
+    var tile2 = board.getTile(x, y);
+    if(tile.status == "ONBOARD)") {
+        if(tile.ownerCompany == null) {
+            results = selectCompany(tile, tile2);
+        }
+        else { /* Fix this */
+            company = tile2.ownerCompany;
+            company.addTile(tile2);
         }
     }
-    if(tile.right) {
-        compare = board.getTile(x, y-1);
-        if(compare.status == "ONBOARD") {
-            if(compare.company == null) {
-                results = selectCompany(tile, compare);
-                console(results);
-            }
-            else {
-                company = compare.company;
-                company.add(compare);
-            }
-        }
-    }
-    if(tile.bottom) {
-        compare = board.getTile(x, y-1);
-        if(compare.status == "ONBOARD") {
-            if(compare.company == null) {
-                results = selectCompany(tile, compare);
-                console(results);
-            }
-            else {
-                company = compare.company;
-                company.add(compare);
-            }
-        }
-    }
-    if(tile.left) {
-        compare = board.getTile(x, y-1);
-        if(compare.status == "ONBOARD") {
-            if(compare.company == null) {
-                results = selectCompany(tile, compare);
-                console(results);
-            }
-            else {
-                company = compare.company;
-                company.add(compare);
-            }
-        }
-    }
+
 }
 
 /* function checkAdjacency helper */
 /* Make a helper that slims down the checkAdjacency function */
 
-function playTile() {
+function playTile(tileInPlay) {
+
+    if(tile.status == "INHAND") {
+        //tileInPlay.ownerPlayer(players[whosTurn]);
+        tileInPlay.statusUpdate(2);
+    }
 
 }
 
-function rearrangePlayerSequence() {
+function rearrangePlayerSequence(first) {
+    var tempPlayers = new Players(players.length);
+
+    for(var i = 0; i < tempPlayers.length; i++) {
+        tempPlayers[i] = players[(first + i) % players.length]
+    }
+    return tempPlayers;
 
 }
 
 function drawStartingTiles() {
 
+   for(var i = 0; i < players.length; i++) {
+       player[i].numHand = 0;
+       for(var j = 0; j < 6; j++) {
+
+           var tempTile = board.bagPop();
+           player[0].addHand(tempTile);
+       }
+   }
+
 }
 
 function whoIsFirst() {
 
+    var plength = players.length;
+    var tempTile;
+    //arrayList??
+    for(var i = 0; i < plength; i++) {
+        tempTile = board.bagPop();
+        playTile(tempTile);
+
+        var pythag = Math.sqrt(tempTile.col * temTile.col
+        + tempTile.row * tempTile * row) + tempTile.col;
+        //arrayList?
+    }
+
 }
+
+
+
+/**
+ * Chooses which player will go first.
+ * @param board
+ * @return
+
+private int whoIsFirst(Grid board) {
+    int pLength = players.length;
+    Tile tempTile;
+    ArrayList<Double> distances = new ArrayList<Double>();
+
+    for (int i = 0; i < pLength; i++) {
+        tempTile = board.bagPop();
+        playTile(tempTile);
+
+        double pythag = Math.sqrt(tempTile.col * tempTile.col
+        + tempTile.row * tempTile.row)
+        + tempTile.col;
+        distances.add(pythag);
+
+        // System.out.print(players[i].name);
+        // System.out.printf(" %.2f \n", distances.get(i));
+    }
+    return distances.indexOf(Collections.min(distances));
+} */
+
 
 
 /*
@@ -156,16 +198,87 @@ function whoIsFirst() {
  */
 function getPlayers() {
 
+    var gate = false;
+    var numPlayers;
+    var name;
+    while(!gate) {
+        numPlayers = prompt("Enter the number of Platers (Max 6): ");
+        if(numPlayers > 1 && numPlayers < 7) {
+            gate = true;
+        }
+
+    }
+
+    players = new Player[numPlayers];
+    for(var i = 0; i < numPlayers; i++) {
+        name = prompt("Name of Player: " + (i+1) + ": ");
+        players[i] = new Player(name, board);
+    }
+
 }
+
+/*/**
+ * Initializes the list of players.
+ * @return
+
+private static Player[] getPlayers() {
+
+    boolean gate = false;
+    while (!gate) {
+        System.out.print("Enter the number of Players (Max 6): ");
+        numPlayers = scan.nextInt();
+        if (numPlayers > 1 && numPlayers < 7) {
+            gate = true;
+        }
+    }
+
+    players = new Player[numPlayers];
+    for (int i = 0; i < numPlayers; i++) {
+        System.out.print("Name of player " + (i + 1) + ": ");
+        String name = scan.next();
+        players[i] = new Player(name, board);
+    }
+
+    return players;
+}*/
 
 
 /**
  * List the companies and their tiers.
  */
 function companyList() {
-    print("Tier 1 Hotel Chains: \n\t Rahoi, Tower");
-    print("Tier 2 Hotel Chains: \n\t American, Worldwide, Festival");
-    print("Tier 3 Hotel Chains: \n\t Continental, Imperial");
+
+    var string;
+    console.log("Tier 1 Hotel Chains: \n\t")
+    if(Rahoi.ownerPlayer == null) {
+        string += "Rahoi, ";
+    }
+    if(Tower.ownerPlayer == null) {
+        string += "Tower";
+    }
+    console.log(string + "\n\n");
+    string = "";
+    console.log("Tier 2 Hotel Chains: \n\t");
+    if(American.ownerPlayer == null) {
+        string += "American, ";
+    }
+    if(Worldwide.ownerPlayer == null) {
+        string += "Wordwide, ";
+    }
+    if(Festival.ownerPlayer == null) {
+        string += "Festival";
+    }
+    console.log(string + "\n\n");
+    string = "";
+    console.log("Tier 3 Hotel Chains: \n\t");
+    if(Continental.ownerPlayer == null) {
+        string += "Continental, "
+    }
+    if(Imperial.ownerPlayer == null) {
+        string += "Imperial";
+    }
+    console.log(string + "\n\n");
+
 }
 
 
@@ -208,5 +321,13 @@ function setCompany(Company, tile1, tile2) {
 }
 
 function initCompanies () {
+
+    Rahoi = new Company("Rahoi", 0, 0, 0);
+    Tower = new Company("Tower", 0, 0, 0);
+    American = new Company("American", 1, 0, 0);
+    Worldwide = new Company("WorldWide", 1, 0, 0);
+    Festival = new Company("Festival", 1, 0, 0);
+    Continental = new Company("Continental", 2, 0, 0);
+    Imperial = new Company("Imperial", 2, 0, 0);
 
 }
